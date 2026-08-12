ce4b1d03c8a9f3c661a11dee3451a0bea65b9f54
Robin Candau <antiz@archlinux.org>
2026-08-12 03:35:38 +0200

    fix: Fix fetch option

--- src/fetch.rs
+++ src/fetch.rs
@@ -2,10 +2,12 @@
 
 use anyhow::Context;
 use gix::progress;
-use gix::remote::Direction;
+use gix::refspec;
+use gix::remote::{Direction, ref_map};
 use std::sync::atomic::AtomicBool;
 
 pub fn fetch_repo(repo: &gix::Repository) -> anyhow::Result<()> {
+    // Set git remote
     let remote = repo
         .find_fetch_remote(None)
         .context("Failed to find Git remote")?;
@@ -16,10 +18,22 @@ pub fn fetch_repo(repo: &gix::Repository) -> anyhow::Result<()> {
     // revise that later if needed.
     let should_interrupt = AtomicBool::new(false);
 
+    // Configure fetch respecs
+    let mut ref_options = ref_map::Options::default();
+    ref_options.extra_refspecs.push(
+        refspec::parse(
+            "+refs/heads/*:refs/heads/*".into(),
+            refspec::parse::Operation::Fetch,
+        )
+        .context("Failed to parse fetch refspec")?
+        .into(),
+    );
+
+    // Fetch changes
     remote
         .connect(Direction::Fetch)
         .context("Failed to connect to Git remote")?
-        .prepare_fetch(progress::Discard, Default::default())
+        .prepare_fetch(progress::Discard, ref_options)
         .context("Failed to prepare Git fetch")?
         .receive(progress::Discard, &should_interrupt)
         .context("Failed to fetch Git repository")?;
